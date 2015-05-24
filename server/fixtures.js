4cbffe70e224dc10d6b70a94689c932feab422e0
Luis Echegaray <mrluisechegaray@gmail.com>
2015-05-24 19:05:33 -0400

    added comment forms and part of helpers

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,19 +1,19 @@
-if (Posts.find().count() === 0) {
-    Posts.insert({
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    });
-
-    Posts.insert({
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    });
-
-    Posts.insert({
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    });
-}
+//if (Posts.find().count() === 0) {
+//    Posts.insert({
+//        title: 'Introducing Telescope',
+//        url: 'http://sachagreif.com/introducing-telescope/'
+//    });
+//
+//    Posts.insert({
+//        title: 'Meteor',
+//        url: 'http://meteor.com'
+//    });
+//
+//    Posts.insert({
+//        title: 'The Meteor Book',
+//        url: 'http://themeteorbook.com'
+//    });
+//}
 
 // Fixture data
 if (Posts.find().count() === 0) {
